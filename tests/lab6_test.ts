4cb3c498de41305ace3f0c32e306106bd1ae31f0
rickonly <rickonly@student.21-school.ru>
2026-03-26 22:42:37 +0700

    fixed expect-typr

--- tests/lab6_test.ts
+++ tests/lab6_test.ts
@@ -1,3 +1,22 @@
 import { test } from 'vitest';
 import { expectTypeOf } from 'expect-type'; 
 import { DeepReadonly, PickedByType, EventHandlers } from '../tc/lab6';
+
+
+test('DeepReadonly: should handle deep nesting and arrays', () => {
+  type Complex = {
+    user: {
+      profile: { name: string };
+      tags: string[];
+    };
+  };
+
+  type Expected = {
+    readonly user: {
+      readonly profile: { readonly name: string };
+      readonly tags: readonly string[]; // Массивы тоже должны стать readonly
+    };
+  };
+
+  expectTypeOf<DeepReadonly<Complex>>().toEqualTypeOf<Expected>();
+});
