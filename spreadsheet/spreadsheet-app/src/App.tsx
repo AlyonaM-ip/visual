c449983b1931c58319f8a5e8e7c8bdcfd16cdf13
rickonly <rickonly@student.21-school.ru>
2026-05-15 13:40:49 +0700

    add init configure

---
+++ spreadsheet/spreadsheet-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Spreadsheet from './components/Spreadsheet/Spreadsheet';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Spreadsheet</h1>
+      <Spreadsheet />
+    </div>
+  );
+}
+
+export default App;
