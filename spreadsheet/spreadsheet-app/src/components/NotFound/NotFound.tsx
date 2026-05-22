a4a9cb17c9be36ca5024893d117e6cb32956bb5e
rickonly <rickonly@student.21-school.ru>
2026-05-22 22:36:09 +0700

    add back link in 404 case

--- spreadsheet/spreadsheet-app/src/components/NotFound/NotFound.tsx
+++ spreadsheet/spreadsheet-app/src/components/NotFound/NotFound.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
   return (
     <div>
       <h2>404</h2>
       <p>Страница не найдена</p>
+      <Link to="/dashboard">На главную</Link>
     </div>
   );
 }
