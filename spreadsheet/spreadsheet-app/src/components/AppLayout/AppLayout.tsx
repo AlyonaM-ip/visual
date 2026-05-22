2b1967976f84f431fa6631038d370121db8ac529
DmitryPolomoshnov <dmitrypolomoshnov3@gmail.com>
2026-05-22 18:47:37 +0300

    logaout + logit button

--- spreadsheet/spreadsheet-app/src/components/AppLayout/AppLayout.tsx
+++ spreadsheet/spreadsheet-app/src/components/AppLayout/AppLayout.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../store/authSlice';
 
 export default function AppLayout() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/login');
+  };
+
   return (
     <div>
-      <h1>Табличный процессор</h1>
+      <h1>SpreadSheet</h1>
+      <button onClick={handleLogout}>Выйти</button>
       <Outlet />
     </div>
   );
